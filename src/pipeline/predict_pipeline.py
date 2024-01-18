153d60b0fff3265447f5116bbeca0721093b4961
DhirajBembade <dhiraj.bembade@gmail.com>
2024-01-18 13:29:22 +0530

    app.py

---
+++ src/pipeline/predict_pipeline.py
@@ -0,0 +1,69 @@
+import os
+import sys
+import pandas as pd
+from src.exception import CustomException
+from src.utils import load_object
+
+class PredictPipeline:
+    def __init__(self):
+        pass
+
+    def predict(self, features):
+        try:
+            # Paths to the saved model and preprocessor
+            model_path = os.path.join("artifacts", "model.pkl")
+            preprocessor_path = os.path.join("artifacts", "preprocessor.pkl")
+
+            # Load the model and preprocessor
+            model = load_object(file_path=model_path)
+            preprocessor = load_object(file_path=preprocessor_path)
+
+            # Transform input features using the preprocessor
+            data_scaled = preprocessor.transform(features)
+
+            # Make predictions using the loaded model
+            preds = model.predict(data_scaled)
+            return preds
+
+        except Exception as e:
+            # Raise a custom exception with details about the error
+            raise CustomException(e, sys)
+
+class CustomData:
+    def __init__(
+        self,
+        gender: str,
+        race_ethnicity: str,
+        parental_level_of_education: str,
+        lunch: str,
+        test_preparation_course: str,
+        reading_score: int,
+        writing_score: int,
+    ):
+        self.gender = gender
+        self.race_ethnicity = race_ethnicity
+        self.parental_level_of_education = parental_level_of_education
+        self.lunch = lunch
+        self.test_preparation_course = test_preparation_course
+        self.reading_score = reading_score
+        self.writing_score = writing_score
+
+    def get_data_as_data_frame(self):
+        try:
+            # Create a dictionary from class attributes
+            custom_data_input_dict = {
+                "gender": [self.gender],
+                "race_ethnicity": [self.race_ethnicity],
+                "parental_level_of_education": [self.parental_level_of_education],
+                "lunch": [self.lunch],
+                "test_preparation_course": [self.test_preparation_course],
+                "reading_score": [self.reading_score],
+                "writing_score": [self.writing_score],
+            }
+
+            # Convert the dictionary to a pandas DataFrame
+            return pd.DataFrame(custom_data_input_dict)
+
+        except Exception as e:
+            # Raise a custom exception with details about the error
+            raise CustomException(e, sys)
